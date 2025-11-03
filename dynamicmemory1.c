60b29d0c10a179fefaafbeebec2c4b673720bea7
Nithin M <mnithin570@gmail.com>
2025-11-03 11:19:33 +0530

    Add files via upload

---
+++ dynamicmemory1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *p, n, i;
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    p = (int *)malloc(n * sizeof(int));
+
+    for (i = 0; i < n; i++) scanf("%d", &p[i]);
+    for (i = 0; i < n; i++) printf("%d ", p[i]);
+
+    free(p);
+    return 0;
+}
