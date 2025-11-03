d5ba85e161ba2d3a1976e112d2ca3f349cd414bb
Nithin M <mnithin570@gmail.com>
2025-11-03 15:19:18 +0530

    Add files via upload

---
+++ pointers&functions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int a = 5, b = 10;
+
+    printf("Before swap: a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);  // pass addresses of a and b
+
+    printf("After swap:  a = %d, b = %d\n", a, b);
+
+    return 0;
+}
