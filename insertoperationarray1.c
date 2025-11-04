6f29dcc2799149559af8b41d95adb28533d15fe3
Nithin M <mnithin570@gmail.com>
2025-11-04 16:29:28 +0530

    Add files via upload

---
+++ insertoperationarray1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#define CAP 5
+
+int main()
+{
+    int a[CAP] = {20, 30, 40};
+    int n = 3;
+    int x = 10;
+
+    if(n < CAP)
+    {
+        for(int i = n; i > 0; i--)
+        {
+            a[i] = a[i - 1];
+        }
+        a[0] = x;
+        n++;
+    }
+    else
+    {
+        printf("Array is full\n");
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
+
+
