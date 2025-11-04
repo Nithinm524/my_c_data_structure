85e803b2e361b5361e94b845f0e4a56b0cd272ab
Nithin M <mnithin570@gmail.com>
2025-11-04 19:49:16 +0530

    Add files via upload

---
+++ deleteoperationarray1c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define CAP 5
+
+int main() {
+    int a[CAP] = {20, 30, 40};
+    int n = 3;  // current number of elements
+
+    if (n > 0) {
+        n--; // simply reduce the size to remove last element
+        printf("Deleted element from end.\n");
+    } else {
+        printf("Array is empty.\n");
+    }
+
+    // Print updated array
+    printf("Array after deletion: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
